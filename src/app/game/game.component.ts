7467e393df3b92fb113c2c7ec0339716a43e31f1
Baris Aslan <baris7801@hotmail.de>
2022-09-12 19:59:15 +0200

    card stack & animation added

---
+++ src/app/game/game.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.scss']
+})
+export class GameComponent implements OnInit {
+  takeCardAnimation = false;
+  currentCard = '';
+  game: Game;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
+
+  takeCard() {
+    if (!this.takeCardAnimation) {
+      this.currentCard = this.game.stack.pop();
+      this.takeCardAnimation = true;
+
+      setTimeout(() => {
+        this.takeCardAnimation = false;
+      }, 1500);
+    }
+  }
+}
