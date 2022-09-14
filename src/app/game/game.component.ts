94781c89cb23c840e1be5490001abc09b9a80e38
Baris Aslan <baris7801@hotmail.de>
2022-09-14 20:39:54 +0200

    first playable version

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Game } from 'src/models/game';
-
+import {MatDialog} from '@angular/material/dialog';
+import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
@@ -11,7 +12,7 @@ export class GameComponent implements OnInit {
   currentCard = '';
   game: Game;
 
-  constructor() { }
+  constructor(public dialog: MatDialog ) { }
 
   ngOnInit(): void {
     this.newGame();
@@ -26,10 +27,24 @@ export class GameComponent implements OnInit {
     if (!this.takeCardAnimation) {
       this.currentCard = this.game.stack.pop();
       this.takeCardAnimation = true;
+      this.game.playedCards.push(this.currentCard);
+      
+      this.game.currentPlayer++;
+      this.game.currentPlayer = this.game.currentPlayer % this.game.players.length;
 
       setTimeout(() => {
         this.takeCardAnimation = false;
       }, 1500);
     }
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddPlayerComponent)
+
+    dialogRef.afterClosed().subscribe(name => {
+      if (name && name.length > 0) {
+        this.game.players.push(name);
+      }
+    });
   }
 }
